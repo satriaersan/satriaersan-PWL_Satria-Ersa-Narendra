41b7b8d8c26e6e72883d41ad4ea0a744598e15d5
satriaersan <satriaersanarendra@gmail.com>
2025-03-06 11:36:44 +0700

    tes

---
+++ Laravel_PWL/storage/framework/views/92c9ebabe2e5e977250266c7acda7d0a.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <h1>Hello, <?php echo e($name); ?></h1>
+    <h1>You are <?php echo e($occupation); ?></h1>
+</body>
+
+</html>
+<?php /**PATH E:\laragon\www\Pemrograman_Web_Lanjut\Laravel_PWL\resources\views/blog/hello.blade.php ENDPATH**/ ?>
---
+++ Laravel_PWL/storage/framework/views/d5d1e4db7141b9c197577ef108fc26e8.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PWL 2025</title>
+    </head>
+    <body>
+        <h2>Pemrograman Web Lanjut 2025</h2>
+        <h3>semangat Belajar Laravel 10</h3>
+    </body>
+</html>
+<?php /**PATH E:\laragon\www\Pemrograman_Web_Lanjut\Laravel_PWL\resources\views/welcome.blade.php ENDPATH**/ ?>
